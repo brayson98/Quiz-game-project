fad1d899c8de0ad8e67e074cc9c64735a42131f1
brayson98 <brayson@live.com>
2023-03-06 16:32:08 +0000

    created server api

---
+++ server/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors');
+const apiUrl = 'https://opentdb.com/api.php';
+
+
+const app = express();
+app.use(cors());
+app.use(express.json())
+
+
+
+
+app.get('/api/questions', async (req, res) => {
+    const { category, difficulty } = req.query;
+  
+    try {
+      const response = await fetch(`${apiUrl}?amount=10&category=${category}&difficulty=${difficulty}`);
+      const data = await response.json();
+      res.json(data.results);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Server error');
+    }
+  });
+
+  module.exports = app;
