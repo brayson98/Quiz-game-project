3f8371874e75f9beec853765a8333d4309b3fba2
brayson98 <brayson@live.com>
2023-03-07 11:20:09 +0000

    added login and sign-up functionality to front end

--- client/assets/index.js
+++ client/assets/index.js
@@ -1,4 +1,4 @@
-const apiUrl = "https://opentdb.com/api.php?amount=10";
+const apiUrl = "https://opentdb.com/api.php?amount=3";
 
 const categoriesContainer = document.getElementById("categories");
 const geographyBtn = document.getElementById("geographyBtn");
@@ -55,6 +55,7 @@ async function startQuiz(category) {
           currentQuestionIndex++;
           if (currentQuestionIndex >= questions.length) {
             // End of quiz
+            submitScore(score);
             alert(`Quiz finished. You scored ${score}/${questions.length}.`);
           } else {
             showQuestion(questions[currentQuestionIndex], currentQuestionIndex);
@@ -93,37 +94,32 @@ const loginForm = document.getElementById('login-form');
 const signupForm = document.getElementById('signup-form');
 
 // Event listener for login form submission
-loginForm.addEventListener('submit', (event) => {
-  event.preventDefault();
 
-  const username = document.getElementById('username').value;
-  const password = document.getElementById('password').value;
+function login() {
+  const username = document.getElementById("username-input").value;
+  const password = document.getElementById("password-input").value;
 
-  const response = fetch('http://localhost:3000/login', {
-    method: 'POST',
+  fetch("http://localhost:3000/login", {
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json"
     },
     body: JSON.stringify({ username, password })
   })
-  .then(response => {
-    if (response.ok) {
-      // Login successful
-      return response.json();
-    } else {
-      // Login failed
-      throw new Error('Invalid username or password.');
-    }
-  })
-  .then(data => {
-    // Update UI with user data (e.g. high score)
-    console.log(data);
-  })
-  .catch(error => {
-    // Display error message to user
-    console.error(error);
-  });
-});
+    .then(response => response.json())
+    .then(data => {
+      if (data.message === "Login successful!") {
+        console.log(data);
+          currentUser = {
+            username: username,
+            highScore: data.highScore
+          };
+          displayLoggedIn();
+      } 
+      document.getElementById("login-message").innerHTML = data.message;
+})
+.catch(error => console.error(error));
+}
 
 // Event listener for signup form submission
 signupForm.addEventListener('submit', (event) => {
@@ -164,3 +160,36 @@ signupForm.addEventListener('submit', (event) => {
     console.error(error);
   });
 });
+
+function submitScore(score) {
+  const response = fetch('http://localhost:3000/submit-score', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ username: user.username, score })
+  })
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Failed to submit score.');
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.error(error);
+  });
+}
+
+function displayLoggedIn() {
+  const loginForm = document.getElementById("login-form");
+  loginForm.style.display = "none";
+
+  const welcomeMessage = document.getElementById("welcome");
+  welcomeMessage.textContent = `Welcome, ${currentUser.username}!`;
+
+  const highscore = document.getElementById("highscore");
+  highscore.textContent = `Highscore: ${currentUser.highScore}`;
+}
