e2c68674d5c74e5a5b83427759072896ead20ba9
Rosie <rosie@greyhoundcs.com>
2023-03-07 14:38:31 +0000

    restructured index.js

--- client/assets/index.js
+++ client/assets/index.js
@@ -1,4 +1,4 @@
-const apiUrl = "https://opentdb.com/api.php?amount=10";
+const apiUrl = "https://opentdb.com/api.php?amount=1";
 
 const categoriesContainer = document.getElementById("categories");
 const geographyBtn = document.getElementById("geographyBtn");
@@ -16,64 +16,73 @@ literatureBtn.addEventListener("click", () => startQuiz("literature"));
 
 restartBtn.addEventListener("click", restartQuiz);
 
+let currentQuestionIndex = 0;
+
 async function startQuiz(category) {
   categoriesContainer.style.display = "none";
   questionContainer.style.display = "block";
 
-  const questions = await fetchQuestions(category);
-
-  let currentQuestionIndex = 0;
-  let score = 0;
-  showQuestion(questions[currentQuestionIndex], currentQuestionIndex);
-
-  function showQuestion(question, index) {
-    questionEl.innerText = `${index+1}. ${question.question}`;
-
-    // Shuffle the answers
-    const answers = [...question.incorrect_answers, question.correct_answer];
-    answers.sort(() => Math.random() - 0.5);
-
-    answerListEl.innerHTML = "";
-    answers.forEach(answer => {
-      const li = document.createElement("li");
-      const button = document.createElement("button");
-      button.innerText = answer;
-      button.addEventListener("click", () => {
-        if (answer === question.correct_answer) {
-          score++;
-          scoreEl.innerText = `Score: ${score}`;
-          button.style.backgroundColor = "#a8dadc";
-          btn.style.border = 0;
-          btn.style.color = "white";
-
-        } else {
-          button.style.backgroundColor = "#e63946";
-        }
-        // Disable all buttons after answer is chosen
-        answerListEl.querySelectorAll("button").forEach(btn => {
-          btn.disabled = true;
-          if (btn.innerText === question.correct_answer) {
-            btn.style.backgroundColor = "#a8dadc";
-            btn.style.border = 0;
-            btn.style.color = "white";
-          }
-        });
-        // Go to next question after a delay
-        setTimeout(() => {
-          currentQuestionIndex++;
-          if (currentQuestionIndex >= questions.length) {
-            // End of quiz
-            alert(`Quiz finished. You scored ${score}/${questions.length}.`);
-          } else {
-            showQuestion(questions[currentQuestionIndex], currentQuestionIndex);
-          }
-        }, 1500);
-      });
-      li.appendChild(button);
-      answerListEl.appendChild(li);
-    });
-  }
+  scoreEl.textContent = 0;
+  showQuestion(category, currentQuestionIndex);
 }
+
+async function showQuestion(category, index) {
+  
+  const question = await fetchQuestions(category);
+  questionEl.innerText = `${index+1}. ${question[0].question}`;
+
+  // Shuffle the answers
+  const answers = [...question[0].incorrect_answers, question[0].correct_answer];
+  answers.sort(() => Math.random() - 0.5);
+
+  answerListEl.innerHTML = "";
+  answers.forEach(answer => {
+    const li = document.createElement("li");
+    const button = document.createElement("button");
+    button.innerText = answer;
+    if (answer === question[0].correct_answer) {
+      button.setAttribute("class", "correct");
+    } else {
+      button.setAttribute("class", "incorrect")
+    }
+    button.addEventListener("click", () => {
+      markAnswer(button)
+      setTimeout(() => {
+        if (currentQuestionIndex >= 10) {
+          // End of quiz
+          alert(`Quiz finished. You scored ${scoreEl.textContent}/${currentQuestionIndex}.`);
+        } else {
+          showQuestion(category, currentQuestionIndex);
+        }
+      }, 1500);
+    });
+    li.appendChild(button);
+    answerListEl.appendChild(li);
+  });    
+}
+
+const markAnswer = (button) => {
+  if (button.getAttribute("class") === "correct") {
+    scoreEl.innerText ++
+    button.style.backgroundColor = "#06d6a0";
+    button.style.border = 0;
+    button.style.color = "white";
+  } else {
+    button.style.backgroundColor = "#e63946";
+  }
+  // Disable all buttons after answer is chosen
+  answerListEl.querySelectorAll("button").forEach(btn => {
+    btn.disabled = true;
+    if (btn.getAttribute("class") === "correct") {
+      btn.style.backgroundColor = "#06d6a0";
+      btn.style.border = 0;
+      btn.style.color = "white";
+    }
+  });
+  // Go to next question after a delay
+  currentQuestionIndex++;
+}
+
 function restartQuiz() {
     location.reload();
 }
